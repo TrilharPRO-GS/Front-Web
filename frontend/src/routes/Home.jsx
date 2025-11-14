497b8248061e9a1654d43855d40cb3a041451461
Vinicius <viniciuscavalcantidosreis@gmail.com>
2025-11-14 11:16:23 -0300

    style: footer ajeitado

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -34,7 +34,7 @@ export default function Home({ dark }) { // Recebe 'dark' aqui
   }
 
   return (
-    <div className="max-w-7xl mx-auto p-4">
+    <div className="min-h-screen max-w-7xl mx-auto p-4">
       <SearchBar perfis={perfis} onFilter={applyFilter} dark={dark} />
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
         {filtered.map(p => (
